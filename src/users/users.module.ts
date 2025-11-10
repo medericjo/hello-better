d115eef831038386eb9c4cef5bab9ce95aa60049
medericjo <joel.gbagba@infinity-africa.com>
2025-11-10 16:21:49 +0000

    REF: users moduke

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,29 +1,7 @@
-import { Controller, Get } from "@nestjs/common";
-import {
-  AllowAnonymous,
-  OptionalAuth,
-  Roles,
-  Session,
-  UserSession,
-} from "@thallesp/nestjs-better-auth";
+import { Module } from '@nestjs/common';
+import { UsersController } from './users.controller';
 
-@Controller("users")
-export class UsersController {
-  @Get("public")
-  @AllowAnonymous() // Allow anonymous access (no authentication required)
-  async publicRoute() {
-    return { message: "This route is public" };
-  }
-
-  @Get("optional")
-  @OptionalAuth() // Authentication is optional for this route
-  async optionalRoute(@Session() session: UserSession) {
-    return { authenticated: !!session, session };
-  }
-
-  @Get("admin")
-  @Roles(["admin"]) // Only authenticated users with the 'admin' role can access this route. Uses the access control plugin from better-auth.
-  adminRoute() {
-    return "Only admins can see this";
-  }
-}
+@Module({
+  controllers: [UsersController]
+})
+export class UsersModule {}
